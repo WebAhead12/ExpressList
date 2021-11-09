54d1ebe9391e06588445858ab58ef09115416f0e
mohammadfaur <mohammad.fa@live.com>
2021-11-09 10:38:29 +0200

    took out task status absolute

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
-const cookiePraser = require('cookie-praser');
+const path = require('path');
+const cookiePraser = require('cookie-parser');
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
-server.listen(PORT,)
+server.use(express.static('public'))
+
+server.listen(PORT, () => { console.log(`listening on http://localhost:${PORT}`) })
