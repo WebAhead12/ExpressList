eebb1e1dc8ad1942a75ab6d9bbc56ad3af09d921
mohammadfaur <mohammad.fa@live.com>
2021-11-09 12:59:41 +0200

    express home page and log in setup

--- server.js
+++ server.js
@@ -2,12 +2,43 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cookiePraser = require('cookie-parser');
+const authHandler = require(path.join(__dirname, 'authentication-handler'))
 
 const server = express();
+server.use(express.json());
+server.use(cookiePraser());
+
+server.use((req, res, next) => {
+    const token = req.cookie.account
+    if (token) {
+        const user = authHandler.getTokenUser(req.cookie.account);
+        if (user != '')
+            req.user = user;
+    }
+    next();
+})
+
 
 server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    if (req.user) {
+        res.redirect('/home');
+        return;
+    }
+    res.sendFile(path.join(__dirname, 'public', 'log-in.html'));
 })
+
+server.post('/', (req, res) => {
+    const account = req.body;
+    const token = authHandler.toknifyAccount(account);
+    res.cookie('account', token, { maxAge: 600000 });
+
+})
+
+server.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+
 
 server.use(express.static('public'))
 
